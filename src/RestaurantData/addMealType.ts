38d6dec9141df0cf60f127ef44d392b17aa16865
Tt <tt.taiton@gmail.com>
2024-03-26 20:22:11 +0800

    create add meal type to the restaurant

---
+++ src/RestaurantData/addMealType.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const checkMealType = async () => {
+    // List of meal types
+    const mealTypes = ['coffee','bakery','cake','ice-cream','brunch','one-dish-meal'];
+    for (const mealType of mealTypes) {
+        const meal = await prisma.mealTypes.findUnique({
+            where: { MealType: mealType },
+        });
+
+        if (!meal) {
+            await prisma.mealTypes.create({
+                data: { MealType: mealType },
+            });
+        }
+    }
+}
+const addMealTypeToCafe = async (CafeId:number, mealTypes:string[]) => {
+    for (const mealType of mealTypes) {
+        const meal = await prisma.mealTypes.findUnique({
+            where: { MealType: mealType },
+        });
+        console.log(meal);
+        if (meal) {
+            await prisma.cafeMealTypes.create({
+                data: {
+                    CafeId: CafeId,
+                    MealTypesId: meal.MealTypesId,
+                },
+            });
+        }
+    }
+}
+
+module.exports = addMealTypeToCafe,checkMealType
