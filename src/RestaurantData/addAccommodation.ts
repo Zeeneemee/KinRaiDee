82cbb79e66f82e132757bf2807a9adc865bbd320
Tt <tt.taiton@gmail.com>
2024-03-26 20:21:04 +0800

    create function to add accommodation into restaurant

---
+++ src/RestaurantData/addAccommodation.ts
@@ -0,0 +1,38 @@
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+
+
+const checkAccommodation = async () => {
+    const accommodations : string[] = ['Parking', 'Wifi','vegan', 'work-friendly', 'pet-friendly', 'outdoor' ]
+    for (const accommodation of accommodations){
+        const acc = await prisma.accommodations.findUnique({
+            where: { Accommodation: accommodation },
+        });
+        if (!acc){
+            await prisma.accommodations.create({
+                data: { Accommodation : accommodation },
+            });
+        }
+    }
+}
+
+const addAccommodationToCafe = async (CafeId:number,Accommodation:string[])=>{
+    for (const accommodation of Accommodation){
+        const acc = await prisma.accommodations.findUnique({
+            where: { Accommodation: accommodation },
+        }
+        )
+        console.log(acc);
+        if (acc){
+            await prisma.cafeAccommodations.create({
+                data:{
+                    CafeId: CafeId,
+                    AccommodationsId: acc.AccommodationsId,
+                }
+            })
+        }
+    }
+}
+
+module.exports = addAccommodationToCafe,checkAccommodation
